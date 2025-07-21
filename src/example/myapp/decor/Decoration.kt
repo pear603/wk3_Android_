ed5828dbc54998a4d5ee667ff1245b983e49fe97
pear603 <Sujareepear@gmail.com>
2025-07-21 11:04:22 +0700

    chap 8

---
+++ src/example/myapp/decor/Decoration.kt
@@ -0,0 +1,9 @@
+package example.myapp.decor
+
+data class Decoration(val rocks: String) {
+
+}
+// Here is a data class with 3 properties.
+data class Decoration2(val rocks: String, val wood: String, val diver: String){
+
+}
--- src/example/myapp/main.kt
+++ src/example/myapp/main.kt
@@ -1,11 +1,36 @@
 package example.myapp
 
+import example.myapp.decor.Decoration
+import example.myapp.decor.Decoration2
+
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
     myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
-fun main() {
-    buildAquarium()
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
+fun makeDecorations() {
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+// Assign all properties to variables.
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
+}
+
+fun main () {
+    makeDecorations()
 }
