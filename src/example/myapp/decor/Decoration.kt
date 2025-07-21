7b961b142857836a8f8d5ab0d76197f8e6178011
pear603 <Sujareepear@gmail.com>
2025-07-21 11:08:46 +0700

    END 1

--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -7,3 +7,17 @@ data class Decoration(val rocks: String) {
 data class Decoration2(val rocks: String, val wood: String, val diver: String){
 
 }
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+}
